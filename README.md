3d4c85240eb692744a900e39a15fe674def25588
natgsarabia <natalia.garcia@estudiant.fjaverianas.com>
2025-04-29 18:15:06 +0200

    redactar readme y ultimos ajustes

--- README.md
+++ README.md
@@ -1,3 +1,72 @@
-EXPLICAR CAMBIOS
-
 enlace gitpages: https://natgsarabia.github.io/MP06_UF3_PF_nataliaGarcia/
+
+
+****************************************************************************
+*              ADAPTAR UNA APLACIÓN A DECLARATIVA A REACT                  *
+****************************************************************************
+
+*PRIMEROS PASOS:*
+
+El primer paso que realizaremos para adaptar la aplicación y hacerla enteramente con REACT será eliminar el contigo de index.html, dejando unicamente el <div class="app">, al que le añadiremos también el id="app" y en nuestro nuevo app.js nos recreamos un elemento root.render a partir de este. 
+Este elemento nos retornará dos dos componentes de nuestra pagina:
+    - Aside
+    - Main
+
+Ahora definiremos los conextos que queremos que tenga nuestra aplicación. En este caso como elementos que tienen en común ambos componentes son el usario seleccionado y la lista de usuarios, dejaremos definidos estos dos al inicio de nuestra aplicación, antes de empezar a programar ninguna función, de la siguiente forma:
+    const SelectedUser = React.createContext();
+    const UsersList = React.createContext();
+
+y adaptaremos el return de nuestro componente padre para que quede englobado dentro de estos dos contextos, que dejaremos iniciados como estados. 
+De esta forma, cada vez que se modifiquen se renderizara automaticamente la pagina, actualizando la información a los datos actuales. Y podremos suprimir las funciones render que tenemos en la aplicación versión declarativa.
+
+____________________________________________________________________________
+*CREANDO EL COMPONETE ASIDE*
+
+Este componente es el que se encarga de crear nuevos usarios, por lo tanto nos crearemos un estado newUser, con el que poder ir actualizando la lista de usuarios.  EL handler de este estado lo añadiremos en la función onchange del input "Añadir Usuario", de esta forma se actualizará automaticamente cada vez que escribamos un nuevo nombre. 
+
+Como necesitaremos actualizar tanto el conexto UsersList como el SelectedUser declarados al principio de la pagina, los inicializamos ambos justo después de crear el useState
+
+Y nos crearemos la funcion AddUser. En esta usaremos el handler del conetxto UserList para añadir el nuevo usuario y resteaemos el valor de este a "". 
+
+Por ultimo nos crearemos la funcion toggleTheme() para cambiar el tema de claro a oscuro siempre que hagamos click en el boton Tema. La dejaremos indicada dentro de este elemento en la función onClick()
+
+Ahora solo quedaría replicar la información que teniamos en el aside del index.html en el return de este componente. Como hemos declarado el usuario seleccionado como un estado y contexto, lo podemos usar como condicional para cambiar el formato del tag del nombre del usuario al estar o no seleccionado de la siguiente forma:
+    fontWeight:selectUser == index ? "bold" : "normal"
+
+Podremos hacer lo mismo para mostrar o no el div userInfo, en caso de que el valor de selectUser no sea nulo lo mostraremos y si no lo mantendremos en oculto. 
+Así nos podemos ahorrar también las funciones:
+    - selectedUser()
+    - deselectUser()
+    - updateSidebarInfo()
+
+Ya que el hidden de los elementos y su información se renderizaran solos cada vez que se modifique el estado de selectUser
+
+____________________________________________________________________________
+*CREANDO EL COMPONETE MAIN*
+
+En Main volveremos ha iniciar los contextos igual que el componente anterior y además crearemos el estado Task, ya que es aquí donde las creamos y añadimos a cada usuario. 
+
+Nos crearemos la funcion AddTask()
+En las que al handler de usuarios le pasaremos la lista de usuarios pero mapeada. De esta forma podremos recorrer los usuarios y, cuando se encuentre el usuario con el mismo id que del selectUser añadiremos la nueva tarea.
+
+Esta será la unica función que tendremos en Main. 
+Programaremos el return con los mismos elementos que contenia el main en el html anterior.  
+Los unicos cambios serán en el h1, en el que, igual que en el componente anterior, haremos un condicional para que muestre un titulo u otro en función de si hay o no un usuario seleccionado. 
+Y en caso de haber un usuario seleccionado y existir en la lista, la mapearemos para recorrerla e imprimir las tareas de este. 
+Como el <li> que creamos por cada tarea tiene varios elementos internos lo dejaremos especificado en un componente nuevo. 
+
+Finalmente, en el input de la añadir tarea le pasaremos como value el estado tarea, en la función de onChange() el handler de tarea para actualizarla siempre que se modifique el contenido y en el boton onClick() la función de añadir nueva tarea 
+
+____________________________________________________________________________
+*CREANDO EL COMPONETE TASK*
+
+Nuestro útlimo componente será Taks, en esto no hará falta que declaremos ningún nuevo estado, pero si los dos contextos que tenemos, para que pueda acceder a la lista actual de usuarios y al usuario seleccionado. 
+
+Dentro de este componente programaremos las funciones de modificación de tarea:
+    - toggleTask(): para marcar y desmarcar
+    - deletetask(): para eliminarla
+    - editTask(): para modificarla
+
+En todas ellas haremos el mismo proceso que en addTask, al handles de users le pasaremos la lista mapeada y actualizaremos siempre que el index corresponda al del usuario seleccionado. Menos en el delete, que usaremos la función filter para que devuelva una nueva lista sin esta tarea.
+
+Por ultimo, como hasta ahora, devolveremos los mismos elementos que teniamos en el index.html anterior, para que nos muestre la misma información.
--- app.js
+++ app.js
@@ -5,27 +5,23 @@ root.render(<RenderPage />);
 
 /********************************************************************************************************************/
 // CREAMOS CONTEXTOS
-const ThemeContext = React.createContext();
 const SelectedUser = React.createContext();
+const UsersList = React.createContext();
 
 
 /********************************************************************************************************************/
 //COMPONENTE PADRE/RAIZ/ROOT.RENDER
 function RenderPage() {
-  //iniciamos la lista de usuarios al inicio, el el componente padre para poderselo pasar como
-  //props a los hijos y que siempre que se modifique se renderize
-  const [users, setUsers] = React.useState([]);
 
-
-  // Devolvemos los componentes Aside y Main dentro de los dos contextos creados
+  // Devolvemos los componentes Aside y Main dentro de los tres contextos creados
   return (
     <>
-      <SelectedUser.Provider value={React.useState(null)}>
-        <ThemeContext.Provider value={"light"}>
-          <Aside list={users} handler={setUsers}/>
-        </ThemeContext.Provider >
-        <Main list={users} handler={setUsers} />        
-      </SelectedUser.Provider>
+      <UsersList.Provider value={React.useState([])}>
+        <SelectedUser.Provider value={React.useState(null)}>
+          <Aside/>
+          <Main  />        
+        </SelectedUser.Provider>
+      </UsersList.Provider>
     </>
   );
 }
@@ -33,31 +29,26 @@ function RenderPage() {
 
 /********************************************************************************************************************/
 //COMPONENTE ASIDE
-function Aside({list,handler  }) {
+function Aside() {
 
   //estado para crear un nuevo usuario
   const [newUser, setNewUser] = React.useState("");
 
   //Declaramos  los contextos
-  const theme = React.useContext(ThemeContext);
   const [selectUser,setSelectUser] = React.useContext(SelectedUser);
+  const [users, setUsers] = React.useContext(UsersList);
 
   //FUNCION añadir un nuevo usuario a la lista de usuarios
   function addUser() {
     if (newUser.trim() === "") return;
-    handler([...list,{ name: newUser, tasks: [] }]);
+    setUsers([...users,{ name: newUser, tasks: [] }]);
     setNewUser("");
   };
 
   //FUNCIÓN Cambiar Tema
   function toggleTheme() {
-    if(theme === "light" ){
       document.body.classList.toggle("dark");
-      theme ="dark";
-    }  else {
       document.body.classList.toggle("light");
-      theme ="light";
-    } 
   }
 
   //RETURN , imprimimos el aside en el DOM
@@ -68,7 +59,7 @@ function Aside({list,handler  }) {
       {/* Lista usuarios */}
       <ul id="userList">
         {/* En caso de haber usuarios en la lista los mostramos */}
-        {list.length != 0 && list.map((user, index) => (
+        {users.length != 0 && users.map((user, index) => (
             <li
               style={{fontWeight:selectUser == index ? "bold" : "normal", cursor:"pointer"}}
               key={index}
@@ -87,15 +78,15 @@ function Aside({list,handler  }) {
         onChange={(e) => setNewUser(e.target.value)}
         placeholder="Nuevo usuario"
       />
-      <button onClick={addUser}>Agregar</button>
+      <button onClick={addUser}>Añadir Usuario</button>
       
       {/* informacion del usuario, solo se muestra si hay uno seleccionado */}
       <div id="userInfo" className={selectUser==null ?"hidden" :""}>
         <hr />
-        <p id="userName">{selectUser!=null ? list[selectUser].name:""}</p>
+        <p id="userName">{selectUser!=null ? users[selectUser].name:""}</p>
         <p id="userStats">
           {selectUser!=null && (()=>{
-            const user = list[selectUser];
+            const user = users[selectUser];
             const completed = user.tasks.filter(t => t.completed).length;
             return `Tareas: ${user.tasks.length} / ${completed} completadas`;
           })()}
@@ -112,10 +103,12 @@ function Aside({list,handler  }) {
 
 /********************************************************************************************************************/
 //COMPONENTE MAIN
-function Main({list,handler}) {
+function Main() {
 
   //Declaramos el contexto que necesitamos
   const [selectUser,setSelectUser] = React.useContext(SelectedUser);
+  const [users, setUsers] = React.useContext(UsersList);
+
   //Declaramos un state para tareas
   const [newTask, setNewTask] = React.useState("");
  
@@ -123,7 +116,7 @@ function Main({list,handler}) {
   //FUNCION añadir nueva tarea
   function addTask() {
     if (newTask.trim() === "") return;
-    handler(list.map((user, index) => {
+    setUsers(users.map((user, index) => {
       if (index === selectUser) {
       return {
         ...user,
@@ -137,67 +130,17 @@ function Main({list,handler}) {
     setNewTask("");
   }
   
-  //FUNCION marcar tarea como completada
-  function toggleTask(taskIndex) {
-    handler(list.map((user, indexUser) => {
-      if (indexUser === selectUser) {
-        return{
-          ...user,
-          //mapeamos las tareas del usuario y ponemos el estado contrario del actual
-          tasks: user.tasks.map((task, index)=> index ===taskIndex ? {...task, completed: !task.completed} : task)
-        };
-    }
-    return user;
-    }));
-  }
-  
-  //FUNCION eliminar tarea
-  function deleteTask(taskIndex) {
-    handler(list.map((user, indexUser) => {
-      if (indexUser === selectUser) {
-        return{
-          ...user,
-          tasks : user.tasks.filter((task,index)=>index!== taskIndex)
-        };
-      }
-      return user;
-    }));
-  }
-  
-  //Funcion editar tarea
-  function editTask(taskIndex) {
-    const newText = prompt("Editar tarea:", list[selectUser].tasks[taskIndex].text);
-    if (newText !== null && newText.trim() !== "") {
-      handler(list.map((user, indexUser) => {
-        if (indexUser === selectUser) {
-          return{
-            ...user,
-            //mapeamos las tareas del usuario y ponemos el estado contrario del actual
-            tasks: user.tasks.map((task, index)=> index ===taskIndex ? {...task, text: newText} : task)
-          };
-      }
-      return user;
-      }));
-      }
-  }
-  
   return (
     <main className="main">
       <div className="card">
-        <h1 id="mainTitle">{selectUser==null ? "Selecciona un usuario" : "Tareas de " + list[selectUser].name}</h1>
+        <h1 id="mainTitle">{selectUser==null ? "Selecciona un usuario" : "Tareas de " + users[selectUser].name}</h1>
         
         {/*Esta seccion se muestra solo si hay un usuario seleccionado  */}
         <div id="taskSection" className={selectUser==null ?"hidden" :""}>
           <ul id="taskList">
             {/* En caso de que el usuario tenga tareas las imprimirmos */}
-          {selectUser !== null && list[selectUser] && list[selectUser].tasks.map((task, taskIndex) => (
-              <li key={taskIndex} className={task.completed ? "completed" : ""}>
-                <span onClick={() => toggleTask(taskIndex)}>{task.text}</span>
-                <div className="actions">
-                  <button onClick ={() =>editTask(taskIndex)}>✏️</button>
-                  <button onClick ={() =>deleteTask(taskIndex)}>🗑️</button>
-                </div>
-              </li>
+          {selectUser !== null && users[selectUser] && users[selectUser].tasks.map((task, taskIndex) => (
+              <Task taskIndex={taskIndex} task={task}/>
             ))} 
           </ul>
 
@@ -216,3 +159,65 @@ function Main({list,handler}) {
 }
 
 
+/********************************************************************************************************************/
+//COMPONENTE TASK
+function Task ({taskIndex,task}){
+
+  //Declaramos el contexto que necesitaremos
+  const [users, setUsers] = React.useContext(UsersList);
+  const [selectUser,setSelectUser] = React.useContext(SelectedUser);
+
+  //FUNCION marcar tarea como completada
+  function toggleTask(taskIndex) {
+    setUsers(users.map((user, indexUser) => {
+      if (indexUser === selectUser) {
+        return{
+          ...user,
+          //mapeamos las tareas del usuario y ponemos el estado contrario del actual
+          tasks: user.tasks.map((task, index)=> index ===taskIndex ? {...task, completed: !task.completed} : task)
+        };
+    }
+    return user;
+    }));
+  }
+  
+  //FUNCION eliminar tarea
+  function deleteTask(taskIndex) {
+    setUsers(users.map((user, indexUser) => {
+      if (indexUser === selectUser) {
+        return{
+          ...user,
+          tasks : user.tasks.filter((task,index)=>index!== taskIndex)
+        };
+      }
+      return user;
+    }));
+  }
+  
+  //Funcion editar tarea
+  function editTask(taskIndex) {
+    const newText = prompt("Editar tarea:", users[selectUser].tasks[taskIndex].text);
+    if (newText !== null && newText.trim() !== "") {
+      setUsers(users.map((user, indexUser) => {
+        if (indexUser === selectUser) {
+          return{
+            ...user,
+            //mapeamos las tareas del usuario y ponemos el estado contrario del actual
+            tasks: user.tasks.map((task, index)=> index ===taskIndex ? {...task, text: newText} : task)
+          };
+      }
+      return user;
+      }));
+      }
+  }
+
+  return (
+    <li key={taskIndex} className={task.completed ? "completed" : ""}>
+        <span onClick={() => toggleTask(taskIndex)}>{task.text}</span>
+        <div className="actions">
+          <button onClick ={() =>editTask(taskIndex)}>✏️</button>
+          <button onClick ={() =>deleteTask(taskIndex)}>🗑️</button>
+        </div>
+    </li>
+  );
+}
